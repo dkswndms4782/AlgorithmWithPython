051ecec358f2f0a2d0d20eb53b7ea6170c2ea813
dkswndms4782 <dkswndms4782@naver.com>
2022-06-12 13:17:51 +0100

    second version

--- basic/bfs/1697_숨바꼭질.py
+++ basic/bfs/1697_숨바꼭질.py
@@ -1,33 +1,44 @@
-# N과 K가 같을 때 exit()를 안해줌
-# seen 크기 문제
-# -로 좌표가 가는 문제
 import sys
 from collections import deque
 N,K = map(int, sys.stdin.readline().split())
-if N==K: 
-    print(0)
-    sys.exit()
-seen = [False]*(1000001)
+# if N==K: 
+#     print(0)
+#     sys.exit()
+Max = 200000
+seen = [False] * (Max + 1)
+dist = [-1] * (Max + 1)
 q = deque()
 q.append(N)
 seen[N] = True
-time = 1
-while q:
-    now_level = len(q)
-    while now_level:
-        tmp = q.popleft()
-        if (tmp+1) == K or (tmp-1) == K or (tmp*2) == K:
-            print(time)
-            sys.exit()
-        if not seen[tmp*2] and tmp<=K:
-            seen[tmp*2] = True
-            q.append(tmp*2)
-        if not seen[tmp+1]:
-            seen[tmp+1] = True
-            q.append(tmp+1)
-        if not seen[tmp-1] and (tmp-1) >= 0:
-            seen[tmp-1] = True
-            q.append(tmp-1)
-        now_level -= 1
-    time += 1
+dist[N] = 0
+# 첫번째 버전
+# time = 1
+# while q:
+#     now_level = len(q)
+#     while now_level:
+#         tmp = q.popleft()
+#         if (tmp+1) == K or (tmp-1) == K or (tmp*2) == K:
+#             print(time)
+#             sys.exit()
+#         if not seen[tmp*2] and tmp<=K:
+#             seen[tmp*2] = True
+#             q.append(tmp*2)
+#         if not seen[tmp+1]:
+#             seen[tmp+1] = True
+#             q.append(tmp+1)
+#         if not seen[tmp-1] and (tmp-1) >= 0:
+#             seen[tmp-1] = True
+#             q.append(tmp-1)
+#         now_level -= 1
+#     time += 1
 
+# 두번째 버전
+while q:
+    now = q.popleft()
+    for nxt in [now-1, now+1, now*2]:
+        if 0<=nxt<=Max and seen[nxt] == False:
+            seen[nxt] = True
+            dist[nxt] = dist[now] + 1
+            q.append(nxt)
+
+print(dist[K])
