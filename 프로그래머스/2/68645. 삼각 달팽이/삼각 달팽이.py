2281eec53121cc785c4ba1785f2a6be289ac0cac
dkswndms4782 <59716219+dkswndms4782@users.noreply.github.com>
2024-10-31 13:56:27 +0900

    [level 2] Title: 삼각 달팽이, Time: 131.28 ms, Memory: 43.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -0,0 +1,17 @@
+from itertools import chain
+def solution(n):
+    answer = [[0]*i for i in range(1,n+1)]
+    [row, col, cnt] = [-1,0,1]
+    
+    for i in range(n):
+        for _ in range(n-i):
+            if i%3==0:
+                row+=1
+            elif i%3==1:
+                col+=1
+            else:
+                row-=1
+                col-=1
+            answer[row][col]=cnt
+            cnt+=1
+    return list(chain(*answer))
